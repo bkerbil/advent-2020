2ac48f42024564d6036644dad19db693be3050c3
bkerbil <psir@protonmail.com>
2020-12-02 11:16:50 +0200

    [feat] Add solutions for day 02's puzzles

---
+++ src/advent/day_02/solution.clj
@@ -0,0 +1,54 @@
+(ns advent.day-02.solution
+  (:use [criterium.core])
+  (:require [support.reader :as r]
+            [clojure.core.match :as m]))
+
+(defn string->data
+  [text]
+  (let [regex #"^(\d+)-(\d+) ([a-z]){1}: (\w+)$"
+        matches (re-matches regex text)]
+    (let [[_ min max letter password] matches]
+      {:min             (Integer/parseInt min)
+       :max             (Integer/parseInt max)
+       :required-letter (first (seq letter))
+       :password        password})))
+
+(defn contains-required-letters?
+  [{:keys [min max required-letter password]}]
+  (let [matches (->> password seq (filter #(= required-letter %)) count)]
+    (>= max matches min)))
+
+(defn get-letter
+  [password position]
+  (nth (seq password) (dec position)))
+
+(defn valid-letter-in-xor-positions?
+  [{:keys [min max required-letter password]}]
+  (let [first-letter (get-letter password min)
+        second-letter (get-letter password max)
+        first-letter-matches? (= first-letter required-letter)
+        second-letter-matches? (= second-letter required-letter)]
+    (m/match [first-letter-matches? second-letter-matches?]
+             [true false] true
+             [false true] true
+             [true true] false
+             [false false] false)))
+
+(defn solve
+  [f passwords]
+  (->> passwords
+       (map string->data)
+       (map f)
+       (filter true?)
+       count))
+
+(def solve-first (partial solve contains-required-letters?))
+(def solve-second (partial solve valid-letter-in-xor-positions?))
+
+(def passwords (r/read-file-as-vec "input.txt"))
+
+;(println (solve-first passwords))                           ; => 410
+;(println (solve-second passwords))                          ; => 694
+
+;(bench (solve-first passwords))                             ; Execution time mean : 2 ms
+;(bench (solve-second passwords))                            ; Execution time mean : 1 ms
