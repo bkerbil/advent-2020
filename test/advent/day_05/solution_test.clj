2f1ecb78e0dead210d552f10c4bd1ff6351448ed
bkerbil <psir@protonmail.com>
2020-12-06 11:54:16 +0200

    [doc] Change a word

--- test/advent/day_05/solution_test.clj
+++ test/advent/day_05/solution_test.clj
@@ -5,6 +5,6 @@
 (def data (slurp "./src/advent/day_05/input.txt"))
 
 (deftest solve-test
-  (testing "should return right value for first and second puzzles"
+  (testing "should return correct value for first and second puzzles"
     (is (= 955 (solution/solve-first data)))
     (is (= 569 (solution/solve-second data)))))
