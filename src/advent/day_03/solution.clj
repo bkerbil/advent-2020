726814743f9323e6516ac82066cbb10e173f2bac
bkerbil <psir@protonmail.com>
2020-12-06 15:43:43 +0200

    [refactor] Removed dependency on custom reader namespace

--- src/advent/day_03/solution.clj
+++ src/advent/day_03/solution.clj
@@ -1,25 +1,27 @@
 (ns advent.day-03.solution
   (:use [criterium.core])
-  (:require [support.reader :as r]
-            [advent.day-03.transform :as t]))
+  (:require [advent.day-03.transform :as transform]
+            [clojure.string :as str]))
+
+(declare width height)
 
 (defn movement-point
   [[x1 y1] [x2 y2]]
   [(+ x1 x2) (+ y1 y2)])
 
 (defn solve-one
-  [starting-point movement chart]
-  (let [width (count (first chart))
-        height (count chart)]
-    (loop [point starting-point
-           result 0]
-      (let [next-point (movement-point point movement)
-            [x y] next-point
-            x (mod x width)]
-        (if (>= (dec height) y 0)
-          (let [value (get-in chart [y x])]
-            (recur next-point (+ result value)))
-          result)))))
+  ([starting-point movement chart]
+   (let [width (count (first chart))
+         height (count chart)]
+     (solve-one starting-point movement chart 0 width height)))
+  ([point movement chart result width height]
+   (let [next-point (movement-point point movement)
+         [x y] next-point
+         x (mod x width)]
+     (if (>= (dec height) y 0)
+       (let [value (get-in chart [y x])]
+         (recur next-point movement chart (+ result value) width height))
+       result))))
 
 (defn solve-multiple
   [starting-point chart movements]
@@ -27,13 +29,14 @@
             (let [b (solve-one starting-point movement chart)]
               (* acc b))) 1 movements))
 
-;(def chart (->> (r/read-file-as-vec "input.txt") (t/transform)))
-;(def starting-point [0 0])
+(def solve (partial solve-multiple [0 0]))
 
-;(def solve (partial solve-multiple starting-point chart))
+;(def chart (->> (slurp "input.txt")
+;                (str/split-lines)
+;                (transform/transform)))
 
-;(println (solve [[3 1]]))                                   ; 156
-;(println (solve [[1 1] [3 1] [5 1] [7 1] [1 2]]))           ; 3521829480
+;(println (solve chart [[3 1]]))                             ; 156
+;(println (solve chart [[1 1] [3 1] [5 1] [7 1] [1 2]]))     ; 3521829480
 
-;(bench (solve [[3 1]]))                                     ; Execution time mean : 76 µs
-;(bench (solve [[1 1] [3 1] [5 1] [7 1] [1 2]]))             ; Execution time mean : 358 µs
+;(bench (solve chart [[3 1]]))                               ; Execution time mean : 76 µs
+;(bench (solve chart [[1 1] [3 1] [5 1] [7 1] [1 2]]))       ; Execution time mean : 344 µs
--- test/advent/day_03/solution_test.clj
+++ test/advent/day_03/solution_test.clj
@@ -1,7 +1,8 @@
 (ns advent.day-03.solution-test
   (:require [clojure.test :refer :all]
-            [advent.day-03.solution :refer :all]
-            [advent.day-03.transform :as t]))
+            [advent.day-03.solution :as solution]
+            [advent.day-03.transform :as transform]
+            [clojure.string :as str]))
 
 (def example-input ["..##......."
                     "#...#...#.."
@@ -15,9 +16,19 @@
                     "#...##....#"
                     ".#..#...#.#"])
 
-(def example-chart (t/transform example-input))
+(def example-chart (transform/transform example-input))
 
 (deftest solve-multiple-test
   (testing "given example input, should return example result"
-    (is (= 7 (solve-multiple [0 0] example-chart [[3 1]])))
-    (is (= 336 (solve-multiple [0 0] example-chart [[1 1] [3 1] [5 1] [7 1] [1 2]])))))
+    (is (= 7 (solution/solve example-chart [[3 1]])))
+    (is (= 336 (solution/solve example-chart [[1 1] [3 1] [5 1] [7 1] [1 2]])))))
+
+
+(def chart (->> (slurp "./src/advent/day_03/input.txt")
+                (str/split-lines)
+                (transform/transform)))
+
+(deftest solve-test
+  (testing "should return correct value for first and second puzzles"
+    (is (= 156 (solution/solve chart [[3 1]])))
+    (is (= 3521829480 (solution/solve chart [[1 1] [3 1] [5 1] [7 1] [1 2]])))))
