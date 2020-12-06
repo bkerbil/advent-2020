93cec013a9c4237d25b768f34fbbe1ca7c30241c
bkerbil <psir@protonmail.com>
2020-12-06 12:52:04 +0200

    [refactor] Will use more functional approach with built-in functions

--- src/advent/day_06/solution.clj
+++ src/advent/day_06/solution.clj
@@ -1,32 +1,23 @@
 (ns advent.day-06.solution
-  (:use [clojure.pprint]
-        [criterium.core])
+  (:use [criterium.core])
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
-(defn solve-first
-  [input]
+(defn solve
+  [f input]
   (->> (str/split input #"\n\n")
-       (map #(str/replace % "\n" ""))
-       (map set)
+       (map #(map set (str/split-lines %)))
+       (map #(apply f %))
        (map count)
        (reduce +)))
 
-(defn solve-second
-  [input]
-  (->> (str/split input #"\n\n")
-       (map (fn [v]
-              (let [items (str/split-lines v)
-                    into-set (map #(set %) items)
-                    intersection (reduce (fn [acc value] (set/intersection acc value)) into-set)]
-                intersection)))
-       (map count)
-       (reduce +)))
+(def solve-first (partial solve set/union))
+(def solve-second (partial solve set/intersection))
 
 ;(def input (slurp "input.txt"))
 
 ;(println (solve-first input))                               ; 6748
 ;(println (solve-second input))                              ; 3445
 
-;(bench (solve-first input))                                 ; Execution time mean : 2 ms
-;(bench (solve-second input))                                ; Execution time mean : 4 ms
+;(bench (solve-first input))                                 ; Execution time mean : 4 ms
+;(bench (solve-second input))                                ; Execution time mean : 5 ms
