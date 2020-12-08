18d5b2b287976f4c4e1ac214f7a9cd057db4fb0a
bkerbil <psir@protonmail.com>
2020-12-08 16:47:00 +0200

    [refactor] Move state initialization inside the function

--- test/advent/day_08/solution_test.clj
+++ test/advent/day_08/solution_test.clj
@@ -1,6 +1,5 @@
 (ns advent.day-08.solution-test
   (:require [clojure.test :refer :all]
-            [boot-code.machine :as machine]
             [advent.day-08.parser :as parser]
             [advent.day-08.solution :as solution]
             [clojure.string :as str]))
@@ -10,11 +9,7 @@
                        parser/string->instructions))
 (def changes (parser/changes instructions))
 
-(def state (ref instructions :validator machine/state-validator-fn))
-(def pointer (ref 0 :validator machine/pointer-validator-fn))
-(def accumulator (ref 0 :validator machine/accumulator-validator-fn))
-
 (deftest solve-test
   (testing "should return correct value for first and second puzzles"
     (is (= {:accumulator 1801, :halted? true} (solution/solve-first instructions)))
-    (is (= {:accumulator 2060, :halted? false} (solution/solve-second state pointer accumulator changes)))))
+    (is (= {:accumulator 2060, :halted? false} (solution/solve-second instructions changes)))))
