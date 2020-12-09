9c78f7ba37e12eff48cb19a6467a1b2288392595
bkerbil <psir@protonmail.com>
2020-12-09 20:36:31 +0200

    [refactor] Reduce code length

--- src/advent/day_09/sum.clj
+++ src/advent/day_09/sum.clj
@@ -1,16 +1,14 @@
 (ns advent.day-09.sum)
 
-(defn can-sum-to?
-  [numbers to sum]
-  (some #(= sum (+ to %)) numbers))
-
 (defn numbers-summable-to?
   ([numbers target]
    (numbers-summable-to? numbers target numbers))
   ([numbers sum knapsack]
-   (cond (empty? knapsack) false
-         (can-sum-to? numbers (first knapsack) sum) true
-         :else (recur numbers sum (rest knapsack)))))
+   (cond
+     (empty? knapsack) false
+     (some #(= sum (+ (first knapsack) %)) numbers) true
+     :else (recur numbers sum (rest knapsack)))))
+
 
 (defn first-non-sum
   [amount numbers]
