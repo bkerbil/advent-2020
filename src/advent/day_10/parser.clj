ab866037101625fccfcd4d28a585458370d20ac8
bkerbil <psir@protonmail.com>
2020-12-10 16:53:24 +0200

    [feat] Solutions for both puzzles on day 10

---
+++ src/advent/day_10/parser.clj
@@ -0,0 +1,22 @@
+(ns advent.day-10.parser
+  (:require [clojure.string :as str]
+            [clojure.spec.alpha :as s]))
+
+(declare data->numbers)
+
+(s/def ::string string?)
+(s/def ::string-args (s/cat :data ::string))
+(s/def ::numbers (s/coll-of int?))
+
+(s/fdef data->numbers
+        :args ::string-args
+        :ret ::numbers)
+
+(defn data->numbers
+  "Converts rows of numbers as a string to a sequence of integers."
+  [data]
+  {:pre  [(s/valid? ::string data)]
+   :post [(s/valid? ::numbers %)]}
+  (->> data
+       str/split-lines
+       (map #(Long/parseLong %))))
