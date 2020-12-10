a2e4c091b8584b6689b96fa7dad5b4a7a9d24041
bkerbil <psir@protonmail.com>
2020-12-10 16:53:10 +0200

    [feat] Solutions for both puzzles on day 10

--- src/advent/day_10/path.clj
+++ src/advent/day_10/path.clj
@@ -2,8 +2,8 @@
 
 (defn same-path?
   [a b]
-  (loop [a a
-         b b]
+  (loop [a (sort a)
+         b (sort b)]
     (if (or (empty? a) (empty? b))
       true
       (if (= (first a) (first b))
