e4a81e8a6af5b11b3a346a606300943d31b5147b
bkerbil <psir@protonmail.com>
2020-12-11 23:01:43 +0200

    [refactor] Change the data type from keywords to integers (with nil), and 2d-vectors to single vector

--- src/advent/day_11/solution.clj
+++ src/advent/day_11/solution.clj
@@ -3,19 +3,19 @@
   (:require [advent.day-11.parser :as parser]
             [advent.day-11.chart :as chart]))
 
+;(stest/enumerate-namespace 'advent.day-11.chart)
+
 (defn solve-first
-  [chart]
-  (let [width (count (first chart))
-        height (count chart)
-        coordinates (for [y (range height)
-                          x (range width)]
-                      [x y])
-        stable-chart (chart/update-chart-until-stable chart coordinates)
-        result (->> stable-chart flatten frequencies (:occupied))]
-    result))
+  [width height chart]
+  (let [stable-chart (chart/update-chart-until-stable chart width height)
+        result (->> stable-chart frequencies)]
+    (get result 1)))
+
+(def width 98)
+(def height 93)
 
 ;(def chart (->> "input.txt" slurp parser/data->chart))
 
-;(println (solve-first chart))                               ; 2338
+;(println (solve-first width height chart))                  ; 2338
 
-;(bench (solve-first chart))                                 ; 4 seconds
+;(bench (solve-first width height chart))                    ; 2 sec
--- test/advent/day_11/solution_test.clj
+++ test/advent/day_11/solution_test.clj
@@ -3,18 +3,17 @@
             [advent.day-11.parser :as parser]
             [advent.day-11.solution :as solution]))
 
-(def numbers (->> "./src/advent/day_11/input.txt" slurp parser/data->chart))
-
 (deftest solve-test
-  (testing "should return correct value for first and second puzzles"
-    (is (= 2338 (solution/solve-first numbers)))
-    ;(is (= 3543369523456 (solution/solve-second numbers)))
-    ))
-
-(def numbers-example-1 (parser/data->chart "L.LL.LL.LL\nLLLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLLL\nL.LLLLLL.L\nL.LLLLL.LL\n"))
+  (let [chart (->> "./src/advent/day_11/input.txt" slurp parser/data->chart)
+        width 98
+        height 93]
+    (testing "should return correct value for first and second puzzles"
+      (is (= 2338 (solution/solve-first width height chart))))))
 
 (deftest known-examples-test
-  (testing "known examples for first puzzle"
-    (is (= 37 (solution/solve-first numbers-example-1))))
-  (testing "known examples for second puzzle"
-    ))
+  (let [chart (parser/data->chart "L.LL.LL.LL\nLLLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLLL\nL.LLLLLL.L\nL.LLLLL.LL\n")
+        width 10
+        height 10]
+    (testing "known examples for first puzzle"
+      (is (= 37 (solution/solve-first width height chart))))
+    (testing "known examples for second puzzle")))
