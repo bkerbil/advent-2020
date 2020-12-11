2630093aea38c19e4967f4e309d9112048bbfc61
bkerbil <psir@protonmail.com>
2020-12-11 23:02:44 +0200

    [refactor] Change the data type from keywords to integers (with nil), and 2d-vectors to single vector

--- src/advent/day_11/rule.clj
+++ src/advent/day_11/rule.clj
@@ -1,18 +1,15 @@
 (ns advent.day-11.rule)
 
-(defn seat-is-empty-and-no-occupied-seats?
-  [point status]
-  (let [result (filter (fn [s] (= s :occupied)) status)]
-    (and (= point :empty) (empty? result))))
+(defn rule?
+  [f1 f2 point status]
+  (and (f1 point) (f2 status)))
 
-(defn seat-is-occupied-and-four-or-more-also-occupied?
-  [point status]
-  (let [result (filter (fn [s] (= s :occupied)) status)]
-    (and (= point :occupied) (>= (count result) 4))))
+(def seat-is-empty-and-no-occupied-seats? (partial rule? #(= % 0) #(= (reduce + %) 0)))
+(def seat-is-occupied-and-four-or-more-also-occupied? (partial rule? #(= % 1) #(>= (reduce + %) 4)))
 
 (defn action
   [point status]
   (cond
-    (seat-is-empty-and-no-occupied-seats? point status) :occupied
-    (seat-is-occupied-and-four-or-more-also-occupied? point status) :empty
+    (seat-is-empty-and-no-occupied-seats? point status) 1
+    (seat-is-occupied-and-four-or-more-also-occupied? point status) 0
     :default point))
