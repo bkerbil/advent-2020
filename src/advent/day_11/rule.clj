705c8709d0b697d639a45637d684193d7edfd11d
bkerbil <psir@protonmail.com>
2020-12-12 16:22:05 +0200

    [feat] Add (very slow) solution for second puzzle on day 11

--- src/advent/day_11/rule.clj
+++ src/advent/day_11/rule.clj
@@ -4,12 +4,26 @@
   [f1 f2 point status]
   (and (f1 point) (f2 status)))
 
+;(def is-zero? #(= % 0))
+;(def is-one? #(= % 1))
+;(def is-zero? #(= (reduce + %) 0))
+
 (def seat-is-empty-and-no-occupied-seats? (partial rule? #(= % 0) #(= (reduce + %) 0)))
 (def seat-is-occupied-and-four-or-more-also-occupied? (partial rule? #(= % 1) #(>= (reduce + %) 4)))
 
-(defn action
+(defn action-adjacent
   [point status]
   (cond
     (seat-is-empty-and-no-occupied-seats? point status) 1
     (seat-is-occupied-and-four-or-more-also-occupied? point status) 0
+    :default point))
+
+(def seat-is-in-line-of-sight-and-no-occupied-seats? (partial rule? #(= % 0) #(= (reduce + %) 0)))
+(def seat-is-occupied-and-five-or-more-also-occupied? (partial rule? #(= % 1) #(>= (reduce + %) 5)))
+
+(defn action-line-of-sight
+  [point status]
+  (cond
+    (seat-is-in-line-of-sight-and-no-occupied-seats? point status) 1
+    (seat-is-occupied-and-five-or-more-also-occupied? point status) 0
     :default point))
