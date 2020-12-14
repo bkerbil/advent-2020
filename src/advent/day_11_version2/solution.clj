ce538ac059f6952cf9740f310b66f9c5ee708ce7
bkerbil <psir@protonmail.com>
2020-12-14 13:00:37 +0200

    [refactor] Simplify with solve-function and defs

--- src/advent/day_11_version2/solution.clj
+++ src/advent/day_11_version2/solution.clj
@@ -1,22 +1,24 @@
 (ns advent.day-11-version2.solution
   (:use [criterium.core])
   (:require [advent.day-11-version2.parser :as parser]
-            [advent.day-11-version2.chart :refer :all]))
+            [advent.day-11-version2.vision :as vision]
+            [advent.day-11-version2.rule :as rule]
+            [advent.day-11-version2.chart :as chart]))
 
 (defn solve
   [neighbours-fn rule-fn state]
-  (->> (stabilize-fn state neighbours-fn rule-fn)
+  (->> (chart/stabilize-fn state neighbours-fn rule-fn)
        flatten
        frequencies
        (#(get % 1))))
 
-(def solve-first (partial solve neighbours-adjacent-fn rule-pre-corona))
-(def solve-second (partial solve neighbours-line-of-sight-fn rule-post-corona))
+(def solve-first (partial solve vision/neighbours-adjacent-fn rule/pre-corona))
+(def solve-second (partial solve vision/neighbours-line-of-sight-fn rule/post-corona))
 
-;(def state (->> "input.txt" slurp parser/data->chart))
+;(def chart (->> "input.txt" slurp parser/data->chart))
 
-;(println (solve-first state))                               ; 2338
-;(println (solve-second state))                              ; 2134
+;(println (solve-first chart))                               ; 2338
+;(println (solve-second chart))                              ; 2134
 
-;(bench (solve-first state))                                 ; Execution time mean : 2.7 sec
-;(bench (solve-second state))                                ; Execution time mean : 3.3 sec
+;(bench (solve-first chart))                                 ; Execution time mean : 2.7 sec
+;(bench (solve-second chart))                                ; Execution time mean : 3.3 sec
