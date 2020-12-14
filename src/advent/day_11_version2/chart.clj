65d80e390b24cd327a5e3d9612fcea679ca66178
bkerbil <psir@protonmail.com>
2020-12-14 13:01:10 +0200

    [feat] Put rules under their own namespace

--- src/advent/day_11_version2/chart.clj
+++ src/advent/day_11_version2/chart.clj
@@ -1,56 +1,22 @@
 (ns advent.day-11-version2.chart)
 
-(declare neighbours-adjacent-fn update-chart-fn rule)
-
-(defn neighbours-adjacent-fn
-  ([chart x y directions]
-   (neighbours-adjacent-fn chart x y directions []))
-  ([chart x y directions result]
-   (if (empty? directions)
-     result
-     (let [[a b] (first directions)
-           dx (+ x a)
-           dy (+ y b)
-           value (get-in chart [dy dx] :out-of-limits)]
-       (cond
-         (= value :out-of-limits) (recur chart x y (rest directions) result)
-         (= value nil) (recur chart x y (rest directions) result)
-         :else (recur chart x y (rest directions) (conj result value)))))))
-
-(defn line-of-sight-fn
-  [chart x y direction]
-  (let [[a b] direction
-        dx (+ x a)
-        dy (+ y b)
-        value (get-in chart [dy dx] :out-of-limits)]
-    (cond
-      (= value :out-of-limits) nil
-      (number? value) value
-      :else (recur chart dx dy direction))))
-
-(defn neighbours-line-of-sight-fn
-  [chart x y directions]
-  (->> directions
-       (map #(line-of-sight-fn chart x y %))
-       (remove nil?)))
+;(declare update-chart-fn)
 
 (defn update-chart-fn
   ([chart coordinates neighbours-fn rule-fn]
-   (update-chart-fn chart coordinates [] neighbours-fn rule-fn))
+   (update-chart-fn chart coordinates (transient []) neighbours-fn rule-fn))
   ([chart coordinates result neighbours-fn rule-fn]
    (if (empty? coordinates)
-     (->> result (partition (count (first chart))) (map vec) vec)
+     (->> (persistent! result) (partition (count (first chart))) (map vec) vec)
      (let [[x y] (first coordinates)
            point (get-in chart [y x] :out-of-limits)
-           ; todo fix this
            directions [[0 -1] [1 -1] [1 0] [1 1] [0 1] [-1 1] [-1 0] [-1 -1]]
            neighbours (neighbours-fn chart x y directions)
            freqs (frequencies neighbours)
            empty (get freqs 0 0)
            occupied (get freqs 1 0)
            value (rule-fn {:point point :empty empty :occupied occupied})]
-       (recur chart (rest coordinates) (conj result value) neighbours-fn rule-fn)
-       ))))
+       (recur chart (rest coordinates) (conj! result value) neighbours-fn rule-fn)))))
 
 (defn stabilize-fn
   [chart neighbours-fn rule-fn]
@@ -64,17 +30,3 @@
         (if (= current-chart updated-chart)
           current-chart
           (recur updated-chart))))))
-
-(defn rule-pre-corona
-  [{:keys [point occupied]}]
-  (cond
-    (nil? point) point
-    (= 0 point) (if (zero? occupied) 1 0)
-    (= 1 point) (if (>= occupied 4) 0 1)))
-
-(defn rule-post-corona
-  [{:keys [point occupied]}]
-  (cond
-    (nil? point) point
-    (= 0 point) (if (zero? occupied) 1 0)
-    (= 1 point) (if (>= occupied 5) 0 1)))
--- test/advent/day_11_version2/chart_test.clj
+++ test/advent/day_11_version2/chart_test.clj
@@ -1,9 +1,10 @@
 (ns advent.day-11-version2.chart_test
   (:require [clojure.test :refer :all]
             [advent.day-11-version2.protocols :refer :all]
-            [advent.day-11-version2.chart :refer :all]
             [advent.day-11-version2.parser :as parser]
-            [advent.day-11-version2.visualizer :as visualizer]))
+            [advent.day-11-version2.chart :refer :all]
+            [advent.day-11-version2.vision :refer :all]
+            [advent.day-11-version2.rule :refer :all]))
 
 (def chart-0 (parser/data->chart "L.LL.LL.LL\nLLLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLLL\nL.LLLLLL.L\nL.LLLLL.LL\n"))
 (def chart-1 (parser/data->chart "#.##.##.##\n#######.##\n#.#.#..#..\n####.##.##\n#.##.##.##\n#.#####.##\n..#.#.....\n##########\n#.######.#\n#.#####.##"))
@@ -15,57 +16,10 @@
 (def chart-0-post-corona (parser/data->chart "L.LL.LL.LL\nLLLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLLL\nL.LLLLLL.L\nL.LLLLL.LL"))
 (def chart-1-post-corona (parser/data->chart "#.##.##.##\n#######.##\n#.#.#..#..\n####.##.##\n#.##.##.##\n#.#####.##\n..#.#.....\n##########\n#.######.#\n#.#####.##"))
 (def chart-2-post-corona (parser/data->chart "#.LL.LL.L#\n#LLLLLL.LL\nL.L.L..L..\nLLLL.LL.LL\nL.LL.LL.LL\nL.LLLLL.LL\n..L.L.....\nLLLLLLLLL#\n#.LLLLLL.L\n#.LLLLL.L#"))
-
-
 (def chart-3-post-corona (parser/data->chart "#.L#.##.L#\n#L#####.LL\nL.#.#..#..\n##L#.##.##\n#.##.#L.##\n#.#####.#L\n..#.#.....\nLLL####LL#\n#.L#####.L\n#.L####.L#"))
 (def chart-4-post-corona (parser/data->chart "#.L#.L#.L#\n#LLLLLL.LL\nL.L.L..#..\n##LL.LL.L#\nL.LL.LL.L#\n#.LLLLL.LL\n..L.L.....\nLLLLLLLLL#\n#.LLLLL#.L\n#.L#LL#.L#"))
 (def chart-5-post-corona (parser/data->chart "#.L#.L#.L#\n#LLLLLL.LL\nL.L.L..#..\n##L#.#L.L#\nL.L#.#L.L#\n#.L####.LL\n..#.#.....\nLLL###LLL#\n#.LLLLL#.L\n#.L#LL#.L#"))
 (def chart-6-post-corona (parser/data->chart "#.L#.L#.L#\n#LLLLLL.LL\nL.L.L..#..\n##L#.#L.L#\nL.L#.LL.L#\n#.LLLL#.LL\n..#.L.....\nLLL###LLL#\n#.LLLLL#.L\n#.L#LL#.L#"))
-
-
-
-(deftest neighbours-test
-  (let [directions [[0 -1] [1 -1] [1 0] [1 1] [0 1] [-1 1] [-1 0] [-1 -1]]]
-    (testing "neighbours-adjacent-fn"
-      (testing "state-0"
-        (is (= [0 0] (neighbours-adjacent-fn chart-0 0 0 directions)))
-        (is (= [0 0 0 0 0] (neighbours-adjacent-fn chart-0 1 0 directions)))
-        (is (= [0 0 0 0] (neighbours-adjacent-fn chart-0 2 0 directions)))
-        (is (= [0 0 0] (neighbours-adjacent-fn chart-0 9 0 directions)))
-        (is (= [0 0 0] (neighbours-adjacent-fn chart-0 9 1 directions)))
-        (is (= [0] (neighbours-adjacent-fn chart-0 0 9 directions)))
-        (is (= [0 0] (neighbours-adjacent-fn chart-0 9 9 directions)))
-        (is (= [0 0 0 0 0 0 0 0] (neighbours-adjacent-fn chart-0 4 8 directions)))
-        (is (= [0 0 0 0 0] (neighbours-adjacent-fn chart-0 5 5 directions)))))
-
-    (testing "neighbours-line-of-sight-fn"
-      (testing "state-0"
-        (is (= [0 0 0 0 0 0] (neighbours-line-of-sight-fn chart-0-post-corona 7 2 directions)))))))
-
-(deftest rule-test
-  (testing "rule"
-    (testing "empty [chair]"
-      (testing "there is more than one adjacent occupied seat"
-        (is (= 0 (rule-pre-corona {:point 0 :occupied 1})))
-        (is (= 0 (rule-pre-corona {:point 0 :occupied 4})))
-        (is (= 0 (rule-pre-corona {:point 0 :occupied 5})))
-        (is (= 0 (rule-pre-corona {:point 0 :occupied 8}))))
-      (testing "there is no occupied adjacent seats")
-      (is (= 1 (rule-pre-corona {:point 0 :occupied 0}))))
-    (testing "occupied [chair]"
-      (testing "there is four or more adjacent occupied seats"
-        (is (= 0 (rule-pre-corona {:point 1 :occupied 4})))
-        (is (= 0 (rule-pre-corona {:point 1 :occupied 5})))
-        (is (= 0 (rule-pre-corona {:point 1 :occupied 7})))
-        (is (= 0 (rule-pre-corona {:point 1 :occupied 8}))))
-      (testing "there is less than four adjacent occupied seats"
-        (is (= 1 (rule-pre-corona {:point 1 :occupied 0})))
-        (is (= 1 (rule-pre-corona {:point 1 :occupied 1})))
-        (is (= 1 (rule-pre-corona {:point 1 :occupied 2})))
-        (is (= 1 (rule-pre-corona {:point 1 :occupied 3})))))
-    (testing "floor"
-      (is (nil? (rule-pre-corona {:point nil}))))))
-
 
 (deftest update-chart-test
   (testing "update-chart"
@@ -73,22 +27,21 @@
                             x (range 10)]
                         [x y])]
       (testing "should move from one example state to another"
-        (is (= chart-1 (update-chart-fn chart-0 coordinates neighbours-adjacent-fn rule-pre-corona)))
-        (is (= chart-2 (update-chart-fn chart-1 coordinates neighbours-adjacent-fn rule-pre-corona)))
-        (is (= chart-3 (update-chart-fn chart-2 coordinates neighbours-adjacent-fn rule-pre-corona)))
-        (is (= chart-4 (update-chart-fn chart-3 coordinates neighbours-adjacent-fn rule-pre-corona)))
-        (is (= chart-5 (update-chart-fn chart-4 coordinates neighbours-adjacent-fn rule-pre-corona))))
-
+        (is (= chart-1 (update-chart-fn chart-0 coordinates neighbours-adjacent-fn pre-corona)))
+        (is (= chart-2 (update-chart-fn chart-1 coordinates neighbours-adjacent-fn pre-corona)))
+        (is (= chart-3 (update-chart-fn chart-2 coordinates neighbours-adjacent-fn pre-corona)))
+        (is (= chart-4 (update-chart-fn chart-3 coordinates neighbours-adjacent-fn pre-corona)))
+        (is (= chart-5 (update-chart-fn chart-4 coordinates neighbours-adjacent-fn pre-corona))))
       (testing "post-corona"
-        (is (= chart-1-post-corona (update-chart-fn chart-0-post-corona coordinates neighbours-line-of-sight-fn rule-post-corona)))
-        (is (= chart-2-post-corona (update-chart-fn chart-1-post-corona coordinates neighbours-line-of-sight-fn rule-post-corona)))
-        (is (= chart-3-post-corona (update-chart-fn chart-2-post-corona coordinates neighbours-line-of-sight-fn rule-post-corona)))
-        (is (= chart-4-post-corona (update-chart-fn chart-3-post-corona coordinates neighbours-line-of-sight-fn rule-post-corona)))
-        (is (= chart-5-post-corona (update-chart-fn chart-4-post-corona coordinates neighbours-line-of-sight-fn rule-post-corona)))
-        (is (= chart-6-post-corona (update-chart-fn chart-5-post-corona coordinates neighbours-line-of-sight-fn rule-post-corona)))))))
+        (is (= chart-1-post-corona (update-chart-fn chart-0-post-corona coordinates neighbours-line-of-sight-fn post-corona)))
+        (is (= chart-2-post-corona (update-chart-fn chart-1-post-corona coordinates neighbours-line-of-sight-fn post-corona)))
+        (is (= chart-3-post-corona (update-chart-fn chart-2-post-corona coordinates neighbours-line-of-sight-fn post-corona)))
+        (is (= chart-4-post-corona (update-chart-fn chart-3-post-corona coordinates neighbours-line-of-sight-fn post-corona)))
+        (is (= chart-5-post-corona (update-chart-fn chart-4-post-corona coordinates neighbours-line-of-sight-fn post-corona)))
+        (is (= chart-6-post-corona (update-chart-fn chart-5-post-corona coordinates neighbours-line-of-sight-fn post-corona)))))))
 
 (deftest stabilize-test
   (testing "stabilize"
     (testing "should stabilize on final state"
-      (is (= chart-5 (stabilize-fn chart-0 neighbours-adjacent-fn rule-pre-corona)))
-      (is (= chart-6-post-corona (stabilize-fn chart-0-post-corona neighbours-line-of-sight-fn rule-post-corona))))))
+      (is (= chart-5 (stabilize-fn chart-0 neighbours-adjacent-fn pre-corona)))
+      (is (= chart-6-post-corona (stabilize-fn chart-0-post-corona neighbours-line-of-sight-fn post-corona))))))
