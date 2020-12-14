4b048fc56c45d0b6877969257866dfe95c4820fb
bkerbil <psir@protonmail.com>
2020-12-14 13:01:52 +0200

    [feat] Put visions under their own namespace

---
+++ src/advent/day_11_version2/vision.clj
@@ -0,0 +1,33 @@
+(ns advent.day-11-version2.vision)
+
+(defn neighbours-adjacent-fn
+  ([chart x y directions]
+   (neighbours-adjacent-fn chart x y directions (transient [])))
+  ([chart x y directions result]
+   (if (empty? directions)
+     (persistent! result)
+     (let [[a b] (first directions)
+           dx (+ x a)
+           dy (+ y b)
+           value (get-in chart [dy dx] :out-of-limits)]
+       (cond
+         (= value :out-of-limits) (recur chart x y (rest directions) result)
+         (= value nil) (recur chart x y (rest directions) result)
+         :else (recur chart x y (rest directions) (conj! result value)))))))
+
+(defn line-of-sight-fn
+  [chart x y direction]
+  (let [[a b] direction
+        dx (+ x a)
+        dy (+ y b)
+        value (get-in chart [dy dx] :out-of-limits)]
+    (cond
+      (= value :out-of-limits) nil
+      (number? value) value
+      :else (recur chart dx dy direction))))
+
+(defn neighbours-line-of-sight-fn
+  [chart x y directions]
+  (->> directions
+       (map #(line-of-sight-fn chart x y %))
+       (remove nil?)))
