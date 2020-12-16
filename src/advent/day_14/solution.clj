fb49367dbdba17897a283c6577d8be0f334acf26
bkerbil <psir@protonmail.com>
2020-12-16 18:45:30 +0200

    [refactor] Use action from new namespace

--- src/advent/day_14/solution.clj
+++ src/advent/day_14/solution.clj
@@ -2,13 +2,14 @@
   (:use [criterium.core])
   (:require [advent.day-14.parser :as p]
             [advent.day-14.protocols :as proto]
-            [advent.day-14.docking-program :refer [->Docking-Program] :as d]))
+            [advent.day-14.docking-program :refer [->Docking-Program] :as d]
+            [advent.day-14.action :as a]))
 
 (defn solve-first
   [instructions program]
   (if (empty? instructions)
     (proto/initialize program)
-    (recur (rest instructions) (d/action (first instructions) program))))
+    (recur (rest instructions) (a/action (first instructions) program))))
 
 ;(def instructions (->> "input.txt" slurp p/string->instructions))
 ;(def docking-program (->Docking-Program {} nil))
