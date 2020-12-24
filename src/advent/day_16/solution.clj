4bee3fbe6d285fa6a6bb9151ba19c93dba75c1ab
bkerbil <psir@protonmail.com>
2020-12-24 09:49:39 +0200

    [feat] Add solution for first problem on day 16

--- src/advent/day_16/solution.clj
+++ src/advent/day_16/solution.clj
@@ -8,7 +8,8 @@
   (->> (ticket/transform-tickets rules tickets)
        (ticket/error-rate)))
 
-;(def rules (->> "rules.txt" slurp parser/rules-parser))
-;(def nearby-tickets (->> "nearby-tickets.txt" slurp parser/tickets-parser))
-;(println (solve-first rules nearby-tickets))                ; 20048
+(def rules (->> "rules.txt" slurp parser/rules-parser))
+(def nearby-tickets (->> "nearby-tickets.txt" slurp parser/tickets-parser))
+
+(println (solve-first rules nearby-tickets))                ; 20048
 ;(bench (solve-first rules nearby-tickets))                  ; Execution time mean : 20 ms
